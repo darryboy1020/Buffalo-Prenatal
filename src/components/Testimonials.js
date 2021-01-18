8c2f6718635a4ce01b7f2bdcb0201d68610be9bc
Darrien Johnson <darrien.johnson@gmail.com>
2021-01-18 11:06:43 -0500

    changed Testimonial- updated avatar

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -6,7 +6,7 @@ import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
 import Avatar from '@material-ui/core/Avatar'
 
-const Testimonials = ({ href, alt = '', quote }) => {
+const Testimonials = ({ href, alt = '', quote, author }) => {
   const { className: containerClass, styles: containerStyle } = css.resolve`
     * {
       background-color: #282e78;
@@ -24,21 +24,23 @@ const Testimonials = ({ href, alt = '', quote }) => {
   return (
     <div className={containerClass}>
       <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <Typography variant='h6' className={containerStyle.title}>
+            {quote}
+          </Typography>
+        </Grid>
         <Grid item xs={3}>
           <div className={imageClass}>
-            <Avatar
-              alt='placeholder pic'
-              src='/public/images/placeholder_avatar.jpg'
-              className={containerClass.large}
-            />
+            <Image href={href} alt={alt} />
           </div>
         </Grid>
         <Grid item xs={9}>
-          <div className={imageClass}>
-            <Typography variant='h6' className={containerStyle.title}>
-              {quote}
-            </Typography>
-          </div>
+          <Typography variant='h6' className={containerStyle.title}>
+            {quote}
+          </Typography>
+          <Typography variant='h5' className={containerStyle.title}>
+            {author}
+          </Typography>
         </Grid>
       </Grid>
       {containerStyle}
